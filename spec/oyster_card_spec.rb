9a53b850d4513ee036195e86cd1c646d84cb1787
marvinmatovu@gmail.com <student@Makerss-MacBook-Pro.local>
2019-07-01 16:42:54 +0100

    step 5 - top-up feature

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -3,7 +3,14 @@ require './lib/OysterCard.rb'
 describe OysterCard do
   describe '.balance' do
     it 'Should have a balance initialized to 0' do
-      expect(subject.balance).to eq 0
+      expect(subject.balance).to eq (0)
+    end
+  end
+
+  describe '#top_up' do
+    it 'Should add to the balance of your oyster card' do
+        subject.top_up(10)
+      expect(subject.balance).to eq 10
     end
   end
 
