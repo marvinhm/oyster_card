fbabb4bea4254b506d8e3169538c3c528880ca90
Student <student@Makerss-MacBook-Pro.local>
2019-07-02 16:52:02 +0100

    up to stage 12

--- lib/OysterCard.rb
+++ lib/OysterCard.rb
@@ -1,11 +1,11 @@
 class OysterCard
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :entry_station
   DEFAULT_LIMIT = 90
   MIN_BALANCE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
+    #@in_journey = false
   end
 
   def top_up(amount)
@@ -13,25 +13,38 @@ class OysterCard
     @balance += amount
   end
 
-  # def deduct(amount)
-  #   raise 'You have insufficient funds' if enough?(amount)
-  #   @balance-=amount
-  # end
 
-  def touch_in
-    raise 'You have already tapped in!' if @in_journey == true
+  def touch_in(entry_station)
+    raise 'You have already tapped in!' if in_journey?
     raise 'You have insufficient funds' if min?
-    @in_journey = true
+    #@in_journey = true
+    @entry_station = entry_station
   end
 
   def touch_out
-    raise 'You have not tapped in!' if @in_journey == false
-    @in_journey = false
+    raise 'You have not tapped in!' if !in_journey?
+    deduct(MIN_BALANCE)
+    @entry_station = nil
+    in_journey?
+  end
+
+  def in_journey?
+    if entry_station
+      true
+    else
+      false
+    end
   end
 
   private
+
+  def deduct(amount)
+   raise 'You have insufficient funds' if enough?(amount)
+   @balance-=amount
+   end
+
   def enough?(amount)
-    @balance < amount
+   @balance < amount
   end
 
   def min?
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -5,6 +5,7 @@ require './lib/OysterCard.rb'
 describe OysterCard do
   minimum_balance = OysterCard::MIN_BALANCE
   maximum_balance = OysterCard::DEFAULT_LIMIT
+  let(:station) { double :station}
   describe '.balance' do
     it 'Should have a balance initialized to 0' do
       expect(subject.balance).to eq 0
@@ -37,35 +38,64 @@ describe OysterCard do
   #   end
   # end
 
+
+
   describe '#touch_in' do
     it 'Should confirm user has started thier journey' do
       subject.top_up(minimum_balance)
-      subject.touch_in
-      expect(subject.in_journey).to eq(true)
+      subject.touch_in(station)
+      expect(subject.in_journey?).to eq(true)
     end
 
     it 'raise error if user is taps in while already in journey' do
       subject.top_up(minimum_balance)
-      subject.touch_in
-      expect { subject.touch_in }.to raise_error('You have already tapped in!')
+      subject.touch_in(station)
+      expect { subject.touch_in(station) }.to raise_error('You have already tapped in!')
     end
 
     it "raise error if balance is less than the minimum fare" do
-      expect {subject.touch_in}.to raise_error("You have insufficient funds")
+      expect {subject.touch_in("shoreditch")}.to raise_error("You have insufficient funds")
     end
+
+    it 'stores the name of the entry station' do
+      subject.top_up(minimum_balance)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
   end
+end
 
   describe '#touch_out' do
     it 'Should confrm user has ended their journey' do
       subject.top_up(minimum_balance)
-      subject.touch_in
+      subject.touch_in(station)
       subject.touch_out
-      expect(subject.in_journey).to eq(false)
+      expect(subject.in_journey?).to eq(false)
     end
 
     it 'raise error if user hasn not tapped in' do
       subject.top_up(minimum_balance)
       expect {subject.touch_out}.to raise_error("You have not tapped in!")
     end
+
+    it 'deducts fare price from balance' do
+       subject.top_up(minimum_balance)
+       subject.touch_in(station)
+       expect {subject.touch_out}.to change {subject.balance}.from(minimum_balance).to(0)
   end
+
+  it 'forgets name of the entry station when tapping out' do
+    subject.top_up(minimum_balance)
+    subject.touch_in(station)
+    subject.touch_out
+    expect(subject.entry_station).to eq nil
+end
+end
+  describe '#in_journey?' do
+    it 'returns the status of the journey' do
+      subject.top_up(minimum_balance)
+      expect(subject.in_journey?).to eq false
+
+    end
+  end
+
 end
