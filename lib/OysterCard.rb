cc8a66eb8e51c35a6fba17eb4d089e1bb0df0ca4
marvinmatovu@gmail.com <student@Makerss-MacBook-Pro.local>
2019-07-02 15:17:28 +0100

    step 9 - Checking minimum balance

--- lib/OysterCard.rb
+++ lib/OysterCard.rb
@@ -1,14 +1,40 @@
 class OysterCard
-  attr_reader :balance
+  attr_reader :balance, :in_journey
   DEFAULT_LIMIT = 90
+  MIN_BALANCE = 1
 
   def initialize
     @balance = 0
-
+    @in_journey = false
   end
 
   def top_up(amount)
     raise 'Top-Up Limit Reached!' if @balance + amount  >= DEFAULT_LIMIT
     @balance += amount
+  end
+
+  # def deduct(amount)
+  #   raise 'You have insufficient funds' if enough?(amount)
+  #   @balance-=amount
+  # end
+
+  def touch_in
+    raise 'You have already tapped in!' if @in_journey == true
+    raise 'You have insufficient funds' if min?
+    @in_journey = true
+  end
+
+  def touch_out
+    raise 'You have not tapped in!' if @in_journey == false
+    @in_journey = false
+  end
+
+  private
+  def enough?(amount)
+    @balance < amount
+  end
+
+  def min?
+    @balance < MIN_BALANCE
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,23 +1,71 @@
+# frozen_string_literal: true
+
 require './lib/OysterCard.rb'
 
 describe OysterCard do
+  minimum_balance = OysterCard::MIN_BALANCE
+  maximum_balance = OysterCard::DEFAULT_LIMIT
   describe '.balance' do
     it 'Should have a balance initialized to 0' do
-      expect(subject.balance).to eq (0)
+      expect(subject.balance).to eq 0
     end
   end
 
   describe '#top_up' do
     it 'Should add to the balance of your oyster card' do
-        subject.top_up(10)
-      expect(subject.balance).to eq 10
+      subject.top_up(minimum_balance)
+      expect(subject.balance).to eq minimum_balance
     end
   end
 
   describe '#top_up' do
     it 'Raises an error if balance exceeds 90' do
+      maximum_balance += 1
+      expect { subject.top_up(maximum_balance) }.to raise_error 'Top-Up Limit Reached!'
+    end
+  end
 
-      expect{subject.top_up(100)}.to raise_error 'Top-Up Limit Reached!'
+  # describe '#deduct' do
+  #   it 'Shoult deduct a fare price from balance' do
+  #     subject.top_up(5)
+  #     expect { subject.deduct(3) }.to change { subject.balance }.from(5).to(2)
+  #   end
+  #
+  #   it 'raises error if fare price exceeds balance' do
+  #     subject.top_up(5)
+  #     expect { subject.deduct(8) }.to raise_error('You have insufficient funds')
+  #   end
+  # end
+
+  describe '#touch_in' do
+    it 'Should confirm user has started thier journey' do
+      subject.top_up(minimum_balance)
+      subject.touch_in
+      expect(subject.in_journey).to eq(true)
+    end
+
+    it 'raise error if user is taps in while already in journey' do
+      subject.top_up(minimum_balance)
+      subject.touch_in
+      expect { subject.touch_in }.to raise_error('You have already tapped in!')
+    end
+
+    it "raise error if balance is less than the minimum fare" do
+      expect {subject.touch_in}.to raise_error("You have insufficient funds")
+    end
+  end
+
+  describe '#touch_out' do
+    it 'Should confrm user has ended their journey' do
+      subject.top_up(minimum_balance)
+      subject.touch_in
+      subject.touch_out
+      expect(subject.in_journey).to eq(false)
+    end
+
+    it 'raise error if user hasn not tapped in' do
+      subject.top_up(minimum_balance)
+      expect {subject.touch_out}.to raise_error("You have not tapped in!")
     end
   end
 end
